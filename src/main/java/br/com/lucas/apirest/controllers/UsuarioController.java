c6bbbcfad5639afb25887189f8aff67b7379ff93
Lucas Valentim <lucas.valentim.costa@gmail.com>
2021-06-15 17:08:24 -0300

    add metodos no controller para salvar, excluir e listar todos

---
+++ src/main/java/br/com/lucas/apirest/controllers/UsuarioController.java
@@ -0,0 +1,58 @@
+package br.com.lucas.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucas.apirest.model.Usuario;
+import br.com.lucas.apirest.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class UsuarioController {
+	
+	//Injeção de dependência
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+    
+	@PostMapping(value = "salvar")
+	@ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+	
+	@DeleteMapping(value = "excluir")
+	@ResponseBody
+	public ResponseEntity<String> excluir(@RequestParam Long idUsuario) {
+		
+		usuarioRepository.deleteById(idUsuario);
+		
+		return new ResponseEntity<String>("Usuário Excluído com Sucesso", HttpStatus.OK);
+	}
+    
+    
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody //Retorna os dados ao corpo da resposta
+    public ResponseEntity<List<Usuario>> getListaUsuario(){
+    	
+    	List<Usuario> lista = usuarioRepository.findAll(); // Retorna todos os registros da tabela Usuario
+    	
+    	return new ResponseEntity<List<Usuario>>(lista, HttpStatus.OK); //retorna um JSON
+    }
+}
