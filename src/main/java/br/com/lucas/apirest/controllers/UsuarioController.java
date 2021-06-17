2d1fea0bd293e2f95d73364530132f79d0e9f0cf
Lucas Valentim <lucas.valentim.costa@gmail.com>
2021-06-17 16:30:47 -0300

    Add metodo no controller para atualizar e buscar por nome

--- src/main/java/br/com/lucas/apirest/controllers/UsuarioController.java
+++ src/main/java/br/com/lucas/apirest/controllers/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -65,4 +66,27 @@ public class UsuarioController {
     	
     	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
     }
+    
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+    	
+    	if (usuario.getId() == null) {
+    		return new ResponseEntity<String>("ID não foi infomardo", HttpStatus.OK);
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    
+    }
+    
+    @GetMapping(value = "buscarPorNome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "nome") String nome){
+    	
+    	List<Usuario> listaUsuario = usuarioRepository.buscarPorNome(nome.trim().toUpperCase());
+    	
+    	return new ResponseEntity<List<Usuario>>(listaUsuario, HttpStatus.OK);
+    }
+    
 }
--- src/main/java/br/com/lucas/apirest/repository/UsuarioRepository.java
+++ src/main/java/br/com/lucas/apirest/repository/UsuarioRepository.java
@@ -1,12 +1,20 @@
 package br.com.lucas.apirest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.lucas.apirest.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	@Transactional(readOnly = true)
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	public List<Usuario> buscarPorNome(String nome);
 	
 	
 	
