00c69b287e7c75d4a697936ea562d8b03bbda540
Lucas Valentim <lucas.valentim.costa@gmail.com>
2021-06-19 17:27:55 -0300

    add funcoes jquery para fazer requisicoes para back-end para excluir registros e editar

--- src/main/java/br/com/lucas/apirest/controllers/UsuarioController.java
+++ src/main/java/br/com/lucas/apirest/controllers/UsuarioController.java
@@ -1,7 +1,6 @@
 package br.com.lucas.apirest.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
